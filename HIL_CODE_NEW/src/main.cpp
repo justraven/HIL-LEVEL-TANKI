1f69dd8ccd0adcccd6b9626294f397c9ed3d8e0f
Alim Satria <alimsatria45@outlook.com>
2022-06-08 16:00:58 +0700

    Initial commit

---
+++ HIL_CODE_NEW/src/main.cpp
@@ -0,0 +1,80 @@
+#include <parsing.h>
+
+float KP, KI, KD, KP_, KI_, KD_;
+float setPoint, level, controlSignal; //distance is feedback sensor
+float error = 0.00, totalError, lastError, deltaError;
+float maxControl = 30;
+float minControl = 0;
+
+unsigned long currentPeriod;
+int period = 1; // delay 1 ms
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+
+    READ_DATA_UNTIL('\n');
+    data.replace(',','.');
+    parseString();
+
+    setPoint = DATA_STR(0).toFloat();
+    level    = DATA_STR(1).toFloat();
+    KP       = DATA_STR(2).toFloat();
+    KI       = DATA_STR(3).toFloat();
+    KD       = DATA_STR(4).toFloat();
+
+  }
+
+  if (millis() >= currentPeriod + period) {
+
+    currentPeriod += period;
+
+    error = setPoint - level;
+    totalError += error;
+
+    if (totalError >= maxControl) {
+      totalError = maxControl;
+    }
+    else if (totalError <= minControl) {
+      totalError = minControl;
+    }
+
+    deltaError = error - lastError;
+
+//    controlSignal = (KP * error) + (KI * totalError * period) + ((KD/period) * deltaError);
+
+    KP_ = KP*error;
+    KI_ = KI*totalError*period;
+    KD_ = (KD/period)*deltaError;
+
+    controlSignal = KP_ + KI_ + KD_;
+
+    if (controlSignal >= maxControl) {
+      controlSignal = maxControl;
+    }
+    else if (controlSignal <= minControl) {
+      controlSignal = minControl;
+    }
+
+    lastError = error;
+
+    //-----SEND DATA TO SERIAL------//
+
+    Serial.println(
+      "P" + String(controlSignal) +
+      "S" + String(error)         +
+      "T" + String(level)         +
+      "A" + String(KP_)           +
+      "B" + String(KI_)           +
+      "C" + String(KD_)
+      );
+  }
+}
